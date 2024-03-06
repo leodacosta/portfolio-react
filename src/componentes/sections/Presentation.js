15c945d7fd6637108ad598403c346fc6917a40ae
odranoel6 <odranoeldacosta6@gmail.com>
2024-03-06 15:19:31 -0300

    versão 3.0
    update

--- src/componentes/sections/Presentation.js
+++ src/componentes/sections/Presentation.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 
 function Presentation(){
     const [text, setText] = useState('');
-    const toRotate = ['Leonardo da Costa', 'Desenvolvedor Front-End'];
+    const toRotate = ['Leonardo da Costa', 'Desenvolvedor'];
     const [loop, setLoop] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
     const period = 100;
@@ -42,10 +42,10 @@ function Presentation(){
             <h4><strong> Bem-vindo ao meu Portfólio. </strong></h4>
             <h1> Olá, eu sou {text} </h1>
             <p> 
-                Sou um aficionado por tecnologia e busco cada vez mais soluções que tragam inovações para o meu dia a dia. <br/>
+                Sou um aficionado por Tecnologia e busco cada vez mais soluções que tragam inovações para o meu dia a dia. <br/>
                 Como estudante da área de Tecnologia, busco aprimorar-me para obter cada dia mais o compromisso <br/>
                 de resolver situações complexas e trazer resultados para as pessoas que buscam o meu trabalho. <br/>
-                Abaixo, se é possível enxergar alguns de meus projetos.
+                Abaixo, se é possível ver alguns de meus projetos.
             </p>
             <ButtonA link='https://github.com/odranoeL6' text='Conecte-se comigo!'/>
         </div>
--- src/componentes/sections/Projects.js
+++ src/componentes/sections/Projects.js
@@ -3,7 +3,10 @@ import styles from './Projects.module.css';
 import Cards from '../elements/Cards';
 import arquitetura from '../../Images/Projects/arquitetura.svg';
 import ccxp from '../../Images/Projects/ccxp.svg';
-import portfolio from '../../Images/Projects/portfolio.svg'
+import portfolio from '../../Images/Projects/portfolio.svg';
+import planejador from '../../Images/Projects/planejador.svg';
+import movielist from '../../Images/Projects/movie-list.svg';
+
 
 function Projects(){
     return (
@@ -12,7 +15,7 @@ function Projects(){
             <Cards
             img={arquitetura}
             title="Tradição em projetos de arquitetura"
-            tech="HTML, CSS e JS"
+            tech="HTML e CSS"
             description="Desenvolvimento de uma Landing Page para formação em Tecnologia"
             repo="https://github.com/odranoeL6/projeto-landing-page-arquitetura"
             site="https://projetosdearquitetura-desafio1.netlify.app/"
@@ -33,7 +36,24 @@ function Projects(){
             repo="https://github.com/odranoeL6/portfolio-react"
             site="https://portfolio-react-rosy-alpha.vercel.app/"
                 />
-            <ButtonB text='Acesse meu repositório' link='https://github.com/odranoeL6?tab=repositories'/>
+            <Cards
+            img={planejador}
+            title="Planejador Diário"
+            tech="React"
+            description="Interface de lista de tarefas sendo um planejador diário, possível de edição de tarefa e seu status"
+            repo="https://github.com/odranoeL6/react-js-interface"
+            site="https://tasklist-bay.vercel.app/"
+                />
+            <Cards
+            img={movielist}
+            title="Movie List"
+            tech="React, SASS, Vite e React Router"
+            description="Lista de filmes criada usando React.js que possibilita o usuário procurar por filmes, utilizando uma API do site The Movie DB"
+            repo="https://github.com/odranoeL6/movie-list"
+            site="https://movie-list-mu-three.vercel.app/"
+                />
+
+            <ButtonB id='button' text='Acesse meu repositório' link='https://github.com/odranoeL6?tab=repositories'/>
         </div>
     )
 }
